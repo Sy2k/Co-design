15cf243803e707a480c32037f627ffacdc711167
Ellen Beatriz Shen <ellenbs@al.insper.edu.br>
2019-09-13 09:39:45 -0300

    .

--- assets/js/secao.js
+++ assets/js/secao.js
@@ -6,16 +6,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
   db.download('/', function(data) {
 
-    /*
-      NUNCA, JAMAIS, EM HIPÓTESE ALGUMA faça
-      replace em múltiplas tags se alguma
-      estiver dentro de outra! Veja o HTML
-      e note que não é o caso das abaixo.
-
-      É sério. Se fizer isso, você realmente
-      vai se arrepender. Você foi avisado.
-    */
-
     context = data['biblioteca'][value]
     coDesReplace('title', context)
 
